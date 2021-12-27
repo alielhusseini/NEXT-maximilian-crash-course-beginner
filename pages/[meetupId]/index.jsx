be34d5962b5e1f088fd42c7685f9a159400fa858
Ali El Husseini <ali.elhusseini@ctsjounieh.com>
2021-12-27 15:54:02 +0200

    commiting

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -69,7 +69,7 @@ export async function getStaticProps(context) { // during build time
 export default function MeetupDetailsPage(props) {
     const router = useRouter()
 
-    if (router.isFallback) return <h1>Loading...</h1> // if fallback is true, if blocking then you won't see it
+    if (router.isFallback) return <h1>Loading...</h1> // if fallback is true, if blocking then you won't see it (if very slow you use true if not fallback)
 
     return ( 
         <>
