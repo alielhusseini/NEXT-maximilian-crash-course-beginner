0665aa6dfb8a42511dfcf984339b8b71db7b930e
Ali El Husseini <76795366+alielhusseini@users.noreply.github.com>
2021-12-29 09:23:19 +0200

    Update MainNavigation.js

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,14 +1,17 @@
 import classes from './MainNavigation.module.css'
 import Link from 'next/link'
 
+// <Link href='/'><a>Home</a></Link>
+// <Link href='/' passHref><h1>Home</h1></Link>
+
 export default function MainNavigation() {
     return (
         <header className={classes.header}>
             <div className={classes.logo}>React Meetups</div>
             <nav>
                 <ul>
-                    <li><Link href='/'>All Meetups</Link></li>
-                    <li><Link href='/new-meetup'>Add New Meetup</Link></li>
+                    <li><Link href='/' passHref>All Meetups</Link></li>
+                    <li><Link href='/new-meetup' passHref>Add New Meetup</Link></li>
                 </ul>
             </nav>
         </header>
